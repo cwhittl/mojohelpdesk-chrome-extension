028d5fdd48922ac1ec738d4a763a3aff2c934104
Whit <chriswhittle@collectivebias.com>
2017-02-02 09:53:07 -0600

    fix build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo find ./ -name ".DS_Store" -depth -exec rm {} \;
-zip -r mojohelpdesk-chrome-extension.zip ./  -x "*.git*" -x "*/\.*" -x "screenshots/*" -x "build.sh"
+find ./ -name ".DS_Store" -depth -exec rm {} \;
+zip -r mojohelpdesk-chrome-extension.zip ./  -x "*.git*" -x "*/\.*" -x "screenshots/*" -x "build.sh" -x ".DS_Store"
