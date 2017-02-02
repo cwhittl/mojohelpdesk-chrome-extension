a9b4ddc7c5fb128fdcde8157724b8d1d1180bd37
Whit <chriswhittle@collectivebias.com>
2017-02-02 10:24:24 -0600

    fixed broken enhanced ui

--- init.js
+++ init.js
@@ -1,5 +1,7 @@
 var cb_mojo_ext = new CB_Mojo_Extension(function(cb_mojo_ext) {
-    if (document.URL.indexOf(cb_mojo_ext.mojo_domain) == -1 && document.URL.indexOf("ma/#/tickets/") == -1 && document.URL.indexOf(cb_mojo_ext.email_domain) == -1) {} else {
+    if (document.URL.indexOf(cb_mojo_ext.mojo_domain) == -1 && document.URL.indexOf("ma/#/tickets/") == -1 && document.URL.indexOf(cb_mojo_ext.email_domain) == -1) {
+      //Do nothing
+    } else {
         console.log("Loading Mojo HelpDesk Extension by Collective Bias");
         chrome.runtime.sendMessage({from: 'mojohelpdesk', cb_mojo_ext: cb_mojo_ext});
     }
