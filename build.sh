fdf479aec87ac2175898f7c1dda0d5764b9c506e
Whit <chriswhittle@collectivebias.com>
2017-02-02 10:25:39 -0600

    rm old zip during build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 find ./ -name ".DS_Store" -depth -exec rm {} \;
+rm mojohelpdesk-chrome-extension.zip
 zip -r mojohelpdesk-chrome-extension.zip ./  -x "*.git*" -x "*/\.*" -x "screenshots/*" -x "build.sh" -x ".DS_Store"
